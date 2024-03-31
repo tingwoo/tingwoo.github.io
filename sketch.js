ec9a8264614e7716dfb291498eb32abc0d5b0a9d
tingwu <tingwu92220205@gmail.com>
2024-03-31 15:46:37 +0800

    added links

--- sketch.js
+++ sketch.js
@@ -4,29 +4,30 @@ let rand;
 // let noiseTexture;
 
 function preload() {
-  // load each shader file (don't worry, we will come back to these!)
-  myShader = loadShader('shader.vert', 'shader.frag');
+    // load each shader file (don't worry, we will come back to these!)
+    myShader = loadShader("shader.vert", "shader.frag");
 }
 
 function setup() {
-  cnv = createCanvas(windowWidth, windowHeight, WEBGL);
-  cnv.position(0, 0); 
-
-  rand = random(0, 300);
-
-  // noiseTexture = loadImage("noise.jpg");
+    cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+    cnv.position(0, 0);
+    rand = random(0, 300);
+    // noiseTexture = loadImage("noise.jpg");
 }
 
 function draw() {
-  shader(myShader);
-  myShader.setUniform('uResolution', [windowWidth/windowHeight, 1]);
-  myShader.setUniform('uTime', millis() / 1000.0 + rand);
-  myShader.setUniform('uRunningTime', millis() / 1000.0);
-  // myShader.setUniform('uTex', noiseTexture);
+    shader(myShader);
+    myShader.setUniform(
+        "uResolution",
+        width > height ? [width / height, 1] : [1, height / width]
+    );
+    myShader.setUniform("uTime", millis() / 1000.0 + rand);
+    myShader.setUniform("uRunningTime", millis() / 1000.0);
+    // myShader.setUniform('uTex', noiseTexture);
 
-  rect(0, 0, windowWidth, windowHeight);
+    rect(0, 0, windowWidth, windowHeight);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight);
 }
